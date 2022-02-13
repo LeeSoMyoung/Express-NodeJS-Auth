2ac72657a4625d83973df67e857fea3bafe48b72
LeeSoMyoung <so4644009@gmail.com>
2022-02-13 12:14:26 +0900

    feat: 회원가입 기능 구현
    middleswares/user.js에서 validRegister 함수를 라우터 미들웨어로 사용하여
    알맞은 회원가입 요청인지 확인하는 작업을 거친다.
    알맞은 회원가입 요청이었다면, 비밀번호 해싱 작업 후
    DB에 올리도록 구현하였다.

--- server/routes/middlewares/register.js
+++ server/routes/middlewares/register.js
@@ -1,7 +1,35 @@
 'use strict';
 
-module.exports=[
+module.exports={
     validRegister : (req,res,next)=>{
-        console.log(req);
+        
+        const id = req.body.id;
+        const username = req.body.username;
+
+        if(!id||id.length<3){
+            return res.status(400).send({
+                message:"아이디가 너무 짧거나 입력되지 않았습니다."
+            });
+        }
+
+        if(!req.body.pw||req.body.pw.length<3){
+            return res.status(400).send({
+                message:"비밀번호가 너무 짧거나 입력되지 않았습니다."
+            });
+        }
+
+        if(!req.body.pw_repeat||req.body.pw_repeat!==req.body.pw){
+            return res.status(400).send({
+                message: "비밀번호 확인란이 입력되지 않았거나 너무 짧습니다."
+            });
+        }
+
+        if(!username){
+            return res.status(400).send({
+                message:"사용자 이름이 입력되지 않았습니다."
+            });
+        }
+
+        next();
     }
-];
+};
--- server/routes/signUp.js
+++ server/routes/signUp.js
@@ -1,14 +1,73 @@
 'use strict';
 
+require('dotenv').config();
+
 const db = require('../../src/lib/db.js');
 
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
 
 const registerMiddleware = require('./middlewares/register.js');
 
-router.post('/',registerMiddleware.validRegister,(req,res)=>{
-    console.log(req);
+router.post('/', registerMiddleware.validRegister, async (req, res) => {
+    const { id, username } = req.body;
+
+    db.query(`
+        SELECT uid FROM USERS
+        WHERE id = ${db.escape(id)}
+    `,
+        (err, result) => {
+            if (err) {
+                // DB 쿼리문에서 발생시
+                throw err;
+                return res.status(400).send({
+                    message: err
+                });
+            }
+
+            if (result.length!==0) {
+                // 유저가 존재할때
+                return res.status(400).send({
+                    message: "이미 존재하는 아이디입니다."
+                });
+            }
+            else {
+                bcrypt.hash(req.body.pw, parseInt(process.env.BCRYPT_NUM),
+                    (bErr, bRes) => {
+                        if (bErr) {
+                            // 비밀번호 해싱 과정에서 오류가 생겼다면
+                            throw bErr;
+                            return res.status(500).send({
+                                message: bErr
+                            });
+                        }
+                        else {
+                            db.query(`
+                        INSERT INTO USERS
+                        (uid, id, pw, username)
+                        VALUES
+                        (${db.escape(uuid.v4())}, ${db.escape(id)}, ${db.escape(bRes)}, ${db.escape(username)})
+                    `,
+                                (insertErr, insertResult) => {
+                                    if (insertErr) {
+                                        // 삽입 이상
+                                        throw insertErr;
+                                        return res.status(500).send({
+                                            message: insertErr
+                                        });
+                                    }
+                                    else {
+                                        return res.status(201).send({
+                                            message: "회원가입이 되었습니다."
+                                        });
+                                    }
+                                });
+                        }
+                    });
+            }
+        });
 });
 
 module.exports = router;
