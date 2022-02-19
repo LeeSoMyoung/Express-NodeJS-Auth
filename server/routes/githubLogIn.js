a5c0d5301df0e1c0fc98d9b46ad291a3a7f9d1c7
LeeSoMyoung <so4644009@gmail.com>
2022-02-19 15:50:26 +0900

    feat: 깃허브 로그인 과정 마무리
    깃허브에서 받아온 토큰과 메인화면에서 필요한 정보를 토큰으로 저장하고
    로그인이 될 수 있도록 작성하였다.

--- server/routes/githubLogIn.js
+++ server/routes/githubLogIn.js
@@ -5,35 +5,54 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
-const db = require('../../src/lib/db.js');
+const res = require('express/lib/response');
 
 // End point 2개
 // /login/github
 // /login/github/callback
 
 
-async function getAccessToken(code){
+const getAccessToken = async (code) => {
     const data = {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SERCRETS,
         code
     };
-    const headers={
-        'Content-Type':'application/json'
+    const headers = {
+        'Content-Type': 'application/json'
     };
-    
-    try{
-        const res = await axios.post('https://github.com/login/oauth/access_token',data, {
+
+    try {
+        const res = await axios.post('https://github.com/login/oauth/access_token', data, {
             headers: headers
-        });  
+        });
         const resData = res['data'];
         const params = new URLSearchParams(resData);
         return params.get('access_token');
     }
-    catch(err){
+    catch (err) {
         throw err;
         console.log(err);
+    }
+}
+
+const getGithubUser = async (accessToken) => {
+    try {
+        const req = await axios.get('https://api.github.com/user', {
+            headers: {
+                Authorization: `bearer ${accessToken}`
+            }
+        });
+        const data = await req['data'];
+        return data;
+    }
+    catch(err){
+        throw err;
+        return res.status(403).message({
+            message: "없는 계정이거나 올바르지 않은 계정입니다."
+        });
     }
 }
 
@@ -45,7 +64,33 @@ router.get('/github', (req, res) => {
 router.get('/github/callback', async (req, res) => {
     const code = req.query.code;
     const accessToken = await getAccessToken(code);
-    console.log(accessToken);
+    const githubUserData = await getGithubUser(accessToken);
+
+    console.log('githubUserData',githubUserData);
+
+    if(!githubUserData){
+        return res.status(500).send({
+            message: "깃허브 유저 데이터를 불러오는데 실패하였습니다."
+        });
+    }
+    else{
+        // github 유저 데이터를 불러왔을 시
+
+        const currentUser = {
+            uid: githubUserData['id'],
+            id: githubUserData['login'],
+            username: githubUserData['name']
+        };
+
+        const token = jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "7d" });
+
+        res.cookie(process.env.COOKIE_NAME, token);
+        
+        return res.status(200).send({
+            message: "로그인 성공",
+            accessToken: token
+        });
+    }
 });
 
 module.exports = router;
